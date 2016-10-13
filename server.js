da27619a45f950a5080507d69dc61ab8bdf443af
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 15:58:53 -0700

    Add mongo connection

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var app = require('./server-config.js');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/shortly', function(err) {
+  !err ? console.log('Connected to monogo database') :
+    console.log('Error connecting to mongo database:', err);
+});
 
 var port = 4568;
 
